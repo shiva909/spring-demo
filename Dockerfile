a910c61ded9e998b4eb0d63bf17db62b62325c6d
Siva Bhupani <siva.bhupani@peopletech.com>
2024-09-07 23:54:47 +0530

    first commit

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+#FROM openjdk:17
+#EXPOSE 8080
+#ADD target/springboot-docker.jar springboot-docker.jar
+#ENTRYPOINT ["java","-jar","/springboot-docker.jar"]
+
+FROM openjdk:8-jre
+
+ENTRYPOINT ["java", "-jar", "/app.jar"]
+
+ARG JAR_FILE
+ADD target/${JAR_FILE} /app.jar
---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,34 @@
+package com.example.demo;
+
+import jakarta.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+	public static Logger logger= LoggerFactory.getLogger(DemoApplication.class);
+
+	@PostConstruct
+	public void init(){
+		logger.info("Application Started......._:)");
+	}
+
+
+	@GetMapping("/")
+	public  String home(){
+		return "Welcome to AWS First Deployment";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
